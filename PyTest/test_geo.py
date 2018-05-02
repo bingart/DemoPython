7313af8fc984ee8a62aa366c0aeea1a57edbe0e7
sunfei <feisun@westwin.com>
2018-05-02 13:56:27 +0800

    update

--- PyTest/test_geo.py
+++ PyTest/test_geo.py
@@ -4,6 +4,7 @@
     THIS is USED FOR GENERATE BINGACCOUNT DIMENSSION
 '''
 import maxminddb
+from file_helper import FileHelper
 
 reader = maxminddb.open_database('GeoLite2-Country.mmdb')
 geo = reader.get('47.88.18.160')
@@ -12,3 +13,18 @@ print (geo)
 country = geo['country']
 countryCode = country['iso_code']
 print (countryCode)
+
+ipList = FileHelper.loadFileList('d:/tmp/stem/ip_uniq.txt')
+countryList = []
+reader = maxminddb.open_database('GeoLite2-Country.mmdb')
+for ip in ipList:
+    geo = reader.get(ip)
+    if geo != None and 'country' in geo:
+        country = geo['country']
+        countryName = geo['country']['names']['en']
+        countryCode = country['iso_code']
+    else:
+        countryCode = 'na'
+    countryList.append(countryName)
+reader.close()
+FileHelper.saveFileList('d:/tmp/stem/country.txt', countryList, 'w')
