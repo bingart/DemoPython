2dcbbdb94473cce90dbe3386c698bbc58501bc48
sunfei <feisun@westwin.com>
2018-05-07 15:05:14 +0800

    update

--- PyLib/data/mysql.sql
+++ PyLib/data/mysql.sql
@@ -8,8 +8,19 @@ CREATE TABLE `node` (
     `state` varchar(64) NOT NULL,		/* OK, ERROR */
     `errorCount` bigint(20) NOT NULL,
     `pathOrder` varchar(64) NOT NULL,	/* EXIT/ENTRY/MIDDLE */
+    `reserve` varchar(1024) NULL,	
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+AUTO_INCREMENT=1;
+CREATE UNIQUE INDEX key_index ON node (finger);
+
+CREATE TABLE `path` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `first` varchar(64) NOT NULL,
+    `last` varchar(64) NOT NULL,
+    `full` varchar(1024) NOT NULL,    	
+    `createTime` datetime NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 AUTO_INCREMENT=1;
 
-CREATE UNIQUE INDEX key_index ON node (finger);
