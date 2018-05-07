8ba6daabb5e08edd2346b36174f4b892ecc6e4a5
sunfei <feisun@westwin.com>
2018-05-07 15:41:24 +0800

    update

--- PyLib/data/mysql.sql
+++ PyLib/data/mysql.sql
@@ -2,13 +2,15 @@ CREATE TABLE `node` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `title` varchar(64) NOT NULL,
     `finger` varchar(64) NOT NULL,
-    `historyPath` varchar(1024) NOT NULL,	
+    `historyPath` varchar(1024) NOT NULL,
+    `position` varchar(64) NOT NULL,
+    `region` varchar(64) NOT NULL,
     `createTime` datetime NOT NULL,
     `updateTime` datetime NOT NULL,
     `state` varchar(64) NOT NULL,		/* OK, ERROR */
     `errorCount` bigint(20) NOT NULL,
     `pathOrder` varchar(64) NOT NULL,	/* EXIT/ENTRY/MIDDLE */
-    `reserve` varchar(1024) NULL,	
+    `reserve` varchar(1024) NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
 AUTO_INCREMENT=1;
@@ -18,7 +20,7 @@ CREATE TABLE `path` (
     `id` bigint(20) NOT NULL AUTO_INCREMENT,
     `first` varchar(64) NOT NULL,
     `last` varchar(64) NOT NULL,
-    `full` varchar(1024) NOT NULL,    	
+    `full` varchar(1024) NOT NULL,
     `createTime` datetime NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
---
+++ PyLib/mysql_helper.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+from log_helper import LogHelper
+import pymysql.cursors
+
+class MySqlHelper:
+    
+    def __init__(self, host, port, dbName, user, password):
+        LogHelper.log("created")
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+        self._dbName = dbName
+        self._connection = pymysql.connect(
+            host = self._host,
+            user = self._user,
+            password = self._password,
+            db = self._dbName,
+            charset = 'utf8mb4',
+            cursorclass = pymysql.cursors.DictCursor)
+
+    def close(self):
+        self._connection.close()
+
+    def executeOne(self, sql, params):
+        with self._connection.cursor() as cursor:
+            # Create a new record
+            cursor.execute(sql, params)
+
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        self._connection.commit()
+
+    def queryOne(self, sql, params):
+        with self._connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            row = cursor.fetchone()
+            return row
+
+    def queryMany(self, sql, params, size):
+        with self._connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            rows = cursor.fetchmany(size)
+            return rows
+
+if __name__ == '__main__':
+    try:
+        helper = MySqlHelper('localhost', 3306, 'traffic', 'wp', 'wp', )
+        sql = "INSERT INTO `node`(`title`, `finger`, `historyPath`, `position`, `region`, `createTime`, `updateTime`, `state`, `errorCount`, `pathOrder`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        helper.executeOne(sql, ('1', '2', 'history path', '45.79.95.201', 'US', '2018-05-07 01:00:00', '2018-05-07 01:00:00', 'OK', 0, 'EXIT'))
+    finally:
+        helper.close()
+        
